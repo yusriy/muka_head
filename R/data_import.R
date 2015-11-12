94dd2e8f945a9e6f6886c2722905af05221a9c55
yusriy <yusriyp@gmail.com>
2015-11-12 21:49:11 +0800

    First commit, created data import script.

---
+++ R/data_import.R
@@ -0,0 +1,11 @@
+###############################################################
+# TITLE: Air-sea interaction of a semi-enclosed tropical ocean:
+# a study on carbon, water, and energy budget
+#
+# Grant: Research University Individual
+# Grant no.:  1001/PTEKIND/811316
+#
+# Author: Yusri Yusup, PhD
+# Date: 2015-11-12
+# 
+###############################################################
